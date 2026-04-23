71c73bf5654602ee088465113dd58057db791759
Victoria Elmander <victoria.elmander@gmail.com>
2026-04-23 13:48:36 +0200

    Updated run_analysis

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+library(here)
+
 source(here("scripts", "01_load_data.R"))
 source(here("scripts", "02_prepare_data.R"))
 source(here("scripts", "03_functions.R"))
--- scripts/01_load_data.R
+++ scripts/01_load_data.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(patchwork)
-library(here)
 library(lmtest)
 
 insurance_cost_raw <- read_csv(
